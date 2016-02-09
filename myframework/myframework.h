e7f837e3b58105e03e5d07f77dfbd413be090e54
CS, Vishwanath <VishwanathCS@mast.com>
2016-02-09 11:43:02 +0530

    first commit

---
+++ myframework/myframework.h
@@ -0,0 +1,19 @@
+//
+//  myframework.h
+//  myframework
+//
+//  Created by CS, Vishwanath on 2/9/16.
+//  Copyright © 2016 Lbrands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myframework.
+FOUNDATION_EXPORT double myframeworkVersionNumber;
+
+//! Project version string for myframework.
+FOUNDATION_EXPORT const unsigned char myframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myframework/PublicHeader.h>
+
+
